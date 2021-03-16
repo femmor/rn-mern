967cfd274b79ff987f02493d8582c0f001cc5c43
Emmanuel Egomson <fegomson@gmail.com>
2021-03-16 18:23:26 -0500

    Reading environment variables

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const app = express();
-
 const PORT = 5000
+
+require('dotenv/config')
+const api = process.env.API_URL
+
 
 app.get("/", (req, res) => {
   res.send("Hello API!")
 })
 
 app.listen(PORT, () => {
+  console.log(api);
   console.log(`Server is running on http://localhost:${PORT}`)
 })
