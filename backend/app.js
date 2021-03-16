d55d9316e4b1a847096d83aea4fd5e391bad6cdd
Emmanuel Egomson <fegomson@gmail.com>
2021-03-16 18:35:25 -0500

    Creating API Calls and JSON Data

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,31 @@
 const express = require("express")
 const app = express();
+const bodyParser = require("body-parser")
 const PORT = 5000
+
+// Middleware
+app.use(bodyParser.json())
 
 require('dotenv/config')
 const api = process.env.API_URL
 
+// Get Products
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: "hair dressing gum",
+    image: "some_url",
+  }
+  res.send(product)
+})
 
-app.get("/", (req, res) => {
-  res.send("Hello API!")
+// Post Products
+app.get(`${api}/products`, (req, res) => {
+  const newProduct = req.body
+  console.log(newProduct)
+  res.send(newProduct)
 })
 
 app.listen(PORT, () => {
-  console.log(api);
   console.log(`Server is running on http://localhost:${PORT}`)
 })
