62edda68459033de317a59d49383e89c3f6a9a3d
Emmanuel Egomson <fegomson@gmail.com>
2021-03-23 14:35:26 -0500

    Created the categories model and schema

--- backend/models/category.js
+++ backend/models/category.js
@@ -4,6 +4,15 @@ const categorySchema = mongoose.Schema({
   name: {
     type: String,
     required: true
+  },
+  color: {
+    type: String
+  },
+  icon: {
+    type: String,
+  },
+  image: {
+    type: String,
   }
 },{
   timestamps: true
