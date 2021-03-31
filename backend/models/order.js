dd35f5d21b2c8f26aaa2fd54a37f98da71f7f7d3
Emmanuel Egomson <fegomson@gmail.com>
2021-03-30 19:40:50 -0500

    Array of Refs, Linking Order to OrderItems & Creating OrderItems on Posting Order

--- backend/models/order.js
+++ backend/models/order.js
@@ -6,6 +6,46 @@ const orderSchema = mongoose.Schema({
     ref: "OrderItem",
     required: true
   }],
+  shippingAddress1: {
+    type: String,
+    required: true
+  },
+  shippingAddress2: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  zip: {
+    type: String,
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true,
+    default: 'Pending'
+  },
+  totalPrice: {
+    type: Number,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', 
+  },
+  dateOrdered: {
+    type: Date,
+    default: Date.now,
+  }
 
 })
 
--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const Order = require('../models/order')
+const OrderItem = require('../models/order-item')
 
 router.get('/', async (req, res) => {
   const ordersList = await Order.find({})
@@ -12,5 +13,39 @@ router.get('/', async (req, res) => {
   res.send(ordersList)
 })
 
+// Create an order
+router.post('/', async(req, res) => {
+  const orderItemsIds = Promise.all(req.body.orderItems.map(async orderItem => {
+    let newOrderItem = new OrderItem({
+      quantity: orderItem.quantity,
+      product: orderItem.product
+    })
+
+    newOrderItem = await newOrderItem.save()
+    return newOrderItem._id
+  }))
+
+  const orderItemsIdsResolved = await orderItemsIds
+
+  let order = new Order({
+    orderItems: orderItemsIdsResolved,
+    shippingAddress1: req.body.shippingAddress1,
+    shippingAddress2: req.body.shippingAddress2,
+    city: req.body.city,
+    zip: req.body.zip,
+    country: req.body.country,
+    phone: req.body.phone,
+    status: req.body.status,
+    totalPrice: req.body.totalPrice,
+    user: req.body.user,
+  })
+
+  order = await order.save()
+  
+  if(!order) {
+    return res.status(404).send('Order can not be created!')
+  }
+  res.send(order)
+})
 
 module.exports = router
